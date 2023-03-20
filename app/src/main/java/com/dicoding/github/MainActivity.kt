1c5398c62842001e3b33871308dff8d87e7f10d4
RizalGusnanda <muchamadrizalgusnanda@gmail.com>
2023-03-21 00:27:49 +0700

    fix detail dan gambar , main activity

--- app/src/main/java/com/dicoding/github/MainActivity.kt
+++ app/src/main/java/com/dicoding/github/MainActivity.kt
@@ -36,6 +36,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
